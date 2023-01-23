d97bd665133a2041291f102fa0c0ac8effe9669a
Getzels De Los Santos <getzels.delossantos@nearsure.net>
2023-01-22 22:11:26 -0400

    W03 Team Activity: Dynamic Header and Footer

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,8 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
