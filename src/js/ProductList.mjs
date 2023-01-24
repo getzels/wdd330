a7df62d553a92c7110ff76bde00040f56cd9b9ea
Getzels N. De Los Santos <get.delossantos@gmail.com>
2023-01-24 02:48:30 -0400

    W03 Individual Activity: Expand Inventory (#5)
    Co-authored-by: Getzels De Los Santos <getzels.delossantos@nearsure.net>

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,8 +2,8 @@ import { renderListWithTemplate } from "./utils.mjs"
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-      <a href="product_pages/index.html?product=${product.Id}">
-        <img src="${product.Image}" alt="${product.Name}">
+      <a href="../product_pages/index.html?product=${product.Id}">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}">
         <h3 class="card__brand">${product.Id}</h3>
         <h2 class="card__name">${product.Brand.Name}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p>
@@ -22,8 +22,9 @@ export default class ProductListing {
 
     async init() {
       // our dataSource will return a Promise...so we can use await to resolve it.
-      const list = await this.dataSource.getData();
+      const list = await this.dataSource.getData(this.category);
       this.renderList(list);
+      document.querySelector("#product-category").innerHTML = "Top Products: " + this.category;
     }
 
     renderList(list) {
---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import productData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
+const category = getParam("category");
+
+const datasource = new productData();
+
+const listElement = document.getElementsByClassName("product-list")[0];
+
+const listProduct = new ProductListing(category, datasource, listElement);
+listProduct.init();
